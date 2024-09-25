b597a54385bd42c1035c30aa5cacfef6abd9fd21
priyashan1 <priyashan@xgengroup.com.au>
2024-09-25 23:49:33 +0530

    index file refactor and othe apis fixing

--- services/courses.js
+++ services/courses.js
@@ -16,33 +16,30 @@ export const getCourseList = async (req, res) => {
 };
 
 export const startNewCourse = async (req, res) => {
-  async (req, res) => {
-    const { courseId } = req.body;
+  const { courseId } = req.body;
 
-    try {
-      const statementKeysPath = `configs/courses.json`;
-      let templateData = fs.readFileSync(statementKeysPath, "utf8");
+  try {
+    const statementKeysPath = `configs/courses.json`;
+    let templateData = fs.readFileSync(statementKeysPath, "utf8");
 
-      templateData = JSON.parse(templateData);
-      const course = templateData.find((course) => course.id == courseId);
+    templateData = JSON.parse(templateData);
+    const course = templateData.find((course) => course.id == courseId);
 
-      if (!course) {
-        return res.status(404).json({ message: "Course not found" });
-      }
-
-      course.userStatus = {
-        ...course.userStatus,
-        startDate: new Date(),
-        completed: false,
-        prcentageCompleted: 0,
-      };
-
-      fs.writeFileSync(statementKeysPath, JSON.stringify(templateData));
-
-      return res.status(200).json({ message: "Course started" });
-    } catch (error) {
-      return res.status(500).json({ message: "Internal server error" });
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
     }
-  };
-};
 
+    course.userStatus = {
+      ...course.userStatus,
+      startDate: new Date(),
+      completed: false,
+      prcentageCompleted: 0,
+    };
+
+    fs.writeFileSync(statementKeysPath, JSON.stringify(templateData));
+
+    return res.status(200).json({ message: "Course started" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
